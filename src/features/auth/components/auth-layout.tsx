e7f5bf2c9d74d171943888680bcbebcf05ac92ed
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-03-26 18:46:22 +0600

    refactor(auth): restructure AuthLayout to include a sticky navigation bar

--- src/features/auth/components/auth-layout.tsx
+++ src/features/auth/components/auth-layout.tsx
@@ -3,8 +3,12 @@ import type { PropsWithChildren } from 'react'
 
 export function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <main className="flex min-h-screen items-center justify-center p-4">
-      <Wrapper maxWidth="48rem">{children}</Wrapper>
+    <main className="flex min-h-screen flex-col">
+      <nav className="sticky top-0 w-full">sfdasdfasdf</nav>
+
+      <div className="flex flex-1 items-center justify-center p-4">
+        <Wrapper maxWidth="48rem">{children}</Wrapper>
+      </div>
     </main>
   )
 }
