8177c3c609936ec2c67d3595474c8ad0602fa350
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-03-26 21:18:42 +0600

    refactor(auth): simplify PasswordInput component by removing autoComplete prop

--- src/features/auth/components/password-input.tsx
+++ src/features/auth/components/password-input.tsx
@@ -2,21 +2,14 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { useState, type ComponentProps } from 'react'
 
-type PasswordInputProps = Omit<
-  ComponentProps<typeof Input>,
-  'type' | 'autoComplete'
->
+type PasswordInputProps = Omit<ComponentProps<typeof Input>, 'type'>
 
 export function PasswordInput({ ...props }: PasswordInputProps) {
   const [isVisible, setIsVisible] = useState(false)
 
   return (
     <div className="relative isolate">
-      <Input
-        {...props}
-        type={isVisible ? 'text' : 'password'}
-        autoComplete="new-password"
-      />
+      <Input {...props} type={isVisible ? 'text' : 'password'} />
 
       <Button
         type="button"
